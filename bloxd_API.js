64fa2ec1128f72f16eb9dcbd4e4a7d6708c76871
Ewias456 <eliaszn.ae@gmail.com>
2025-03-22 14:05:50 -0400

    Block GIver

--- bloxd_API.js
+++ bloxd_API.js
@@ -1,3 +1,4 @@
+
 let wordList=["Hilo","Meow","ehehehehe","SKIbIDI TOILET ON MY MIND YEAH"];
 randomNumber=Math.floor(Math.random() * 4)
 let user="\[⚡\]\[🔧\]Arthur: " + wordList[randomNumber];
@@ -8,3 +9,8 @@ broadcastMessage
 api.setPlayerPose(myId,"zombie")
 
 api.changePlayerIntoSkin(myId,"head","zombie")
+
+api.setItemSlot(myId,0,"Unloaded", 1000,{
+    customDisplayName: "custom name",
+    customDescription: "custom description",
+    });
