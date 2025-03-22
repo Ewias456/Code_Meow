de75447a0d419d8cda5ab56565cf7a35610c3f49
Ewias456 <eliaszn.ae@gmail.com>
2025-03-22 13:33:27 -0400

    Random User Message

---
+++ bloxd_API.js
@@ -0,0 +1,6 @@
+let wordList=["Hilo","Meow","ehehehehe","SKIbIDI TOILET ON MY MIND YEAH"];
+randomNumber=Math.floor(Math.random() * 4)
+let user="\[⚡\]\[🔧\]Arthur: " + wordList[randomNumber];
+api.
+broadcastMessage
+(user)
