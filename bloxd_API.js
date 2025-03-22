fc230065f074bb4d6cafb3473e6448d95f23c469
Ewias456 <eliaszn.ae@gmail.com>
2025-03-22 14:23:40 -0400

    Bubbles

--- bloxd_API.js
+++ bloxd_API.js
@@ -14,3 +14,36 @@ api.setItemSlot(myId,0,"Unloaded", 1000,{
     customDisplayName: "custom name",
     customDescription: "custom description",
     });
+
+    let [x, y, z] = thisPos
+y += 1
+api.playParticleEffect({
+    dir1: [-1, -1, -1],
+    dir2: [1, 1, 1],
+    pos1: [x, y, z],
+    pos2: [x + 1, y + 1, z + 1],
+    texture: "bubble",
+    minLifeTime: 1.2,
+    maxLifeTime: 0.45,
+    minEmitPower: 3,
+    maxEmitPower: 4,
+    minSize: 0.25,
+    maxSize: 0.35,
+    manualEmitCount: 20,
+    gravity: [0, -10, 0],
+    colorGradients: [
+        {
+            timeFraction: 0,
+            minColor: [60, 60, 150, 1],
+            maxColor: [200, 200, 255, 1],
+        },
+    ],
+    velocityGradients: [
+        {
+            timeFraction: 0,
+            factor: 1,
+            factor2: 1,
+        },
+    ],
+    blendMode: 1,
+})
