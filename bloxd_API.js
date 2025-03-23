c7a71de2bb3081705135d113eddf83d5310f3f24
Ewias456 <eliaszn.ae@gmail.com>
2025-03-23 14:29:13 -0400

    Illegal Items

--- bloxd_API.js
+++ bloxd_API.js
@@ -62,3 +62,49 @@ api.applyEffect(myId, "Jump Boost", null, { inbuiltLevel: 1})
 api.scalePlayerMeshNodes(myId, { "TorsoNode":[0.7,0.75,0.7], "HeadMesh":[0.9,0.9,0.9,], "ArmRightMesh":[1.2,1.2,1.2], "ArmLeftMesh":[1.2,1.2,1.2], "LegLeftMesh":[0.75,0.7,0.75], "LegRightMesh":[0.75,0.7,0.75] });
 
 api.scalePlayerMeshNodes(myId, { "TorsoNode":[0.5,0.5,0.5],  "HeadMesh":[3,3,3,],  "ArmRightMesh":[1.2,1.2,1.2],  "ArmLeftMesh":[1.2,1.2,1.2],  "LegLeftMesh":[1,0.9,0.9],  "LegRightMesh":[1,0.9,0.9] });
+
+stackableItems=["Toxin Ball","Timed Spike Bomb Block","Water","Lava",
+"Invisible Solid","Block of Emerald","Iron Watermelon","Block of Lapis Lazuli",
+"Melting Ice","Ice Bridge","Loot Chest","Faction Protector","Beacon","Gravel",
+"Useless Soil","Fireball Block","Iceball Block","Grenade","Toxin Ball Block",
+"Rocket","Super Rocket",
+"Yellowstone","Sponge","Dim Lamp Off","Dim Lamp On","Chiseled Block of Quartz",
+"Hay Bale","temp","UNUSED BLOCK TYPE","Mystery Block","Bouncy Bomb Block",
+"Purple Paintball Explosive",
+"Pink Paintball Explosive","Yellow Paintball Explosive",
+"Red Paintball Explosive","Gray Paintball Explosive","Black Paintball Explosive",
+"Blue Paintball Explosive","Light Blue Paintball Explosive",
+"White Paintball Explosive","Cyan Paintball Explosive",
+"Light Gray Paintball Explosive","Brown Paintball Explosive","Orange Balloon",
+"Blue Balloon",
+"Orange Popup Tower","Blue Popup Tower","Light Blue Popup Tower"
+"Blue Strongbed","Light Blue Strongbed","Red Strongbed","Green Strongbed",
+"Yellow Strongbed","Cyan Strongbed","Orange Strongbed","Gray Strongbed",
+"Pink Strongbed","Lime Strongbed","White Strongbed","Black Strongbed",
+"Brown Strongbed","Purple Strongbed","Light Gray Strongbed","Magenta Strongbed"
+]
+unstackableItems=["Red Paintball Gun","Orange Paintball Gun",
+"Blue Paintball Gun",
+"Orange Heavy Paintball Gun","Artisan Shears","Orange Paint Bow","Cornbread",
+"Artisan Axe"]
+
+
+
+for(x=0;x<stackableItems.length;x++)
+{api.editItemCraftingRecipes(myId,
+stackableItems[x], 
+[{requires:[{items:["Stone"],amt: 1}], 
+  produces: 999, 
+  station:"Potion Table"}])}
+
+for(x=0;x<unstackableItems.length;x++)
+{api.editItemCraftingRecipes(myId,
+unstackableItems[x],
+[{requires:[{items:["Stone"],amt:1}],
+produces: 1,
+station:"Potion Table"}])}
+
+
+
+
+api.log("Use the Potion table nearby to get insane illegal items")
